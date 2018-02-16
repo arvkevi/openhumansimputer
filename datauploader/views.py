c7c231f0f1d0c00b5e904151ca183b12b31d5496
Stephanie Unna <curculio@stephanies-mbp.home>
2018-02-16 16:29:49 +0000

    Adding celery.py, tasks.py, and static files, installing dependancies

--- datauploader/views.py
+++ datauploader/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-import demotemplate.settings
+from django.conf import settings
 
 
 def index(request):
     """
     Starting page for app.
     """
-    context = {'client_id': demotemplate.settings.OH_CLIENT_ID,
-               'oh_proj_page': demotemplate.settings.OH_ACTIVITY_PAGE}
+    context = {'client_id': settings.OH_CLIENT_ID,
+               'oh_proj_page': settings.OH_ACTIVITY_PAGE}
 
     return render(request, 'oh_data_source/index.html', context=context)
