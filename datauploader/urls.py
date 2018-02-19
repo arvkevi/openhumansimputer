81e324cad92be2f99ef08a7689f519f037a335cc
Stephanie Unna <steph.unna@gmail.com>
2018-02-19 15:46:01 +0000

    Removing the index path from urls.py

--- datauploader/urls.py
+++ datauploader/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('complete/', views.complete, name='complete'),
 ],
