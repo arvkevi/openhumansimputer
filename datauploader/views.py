696f266822068a7350c9d547e2dc7e225e294564
Stephanie Unna <steph.unna@gmail.com>
2018-02-20 10:46:52 +0000

    Adding complete view

--- datauploader/views.py
+++ datauploader/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+import logging
+
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 from django.conf import settings
+from .tasks import xfer_to_open_humans
+
+# Set up logging.
+logger = logging.getLogger(__name__)
 
 
 def index(request):
@@ -9,4 +16,32 @@ def index(request):
     context = {'client_id': settings.OH_CLIENT_ID,
                'oh_proj_page': settings.OH_ACTIVITY_PAGE}
 
-    return render(request, 'oh_data_source/index.html', context=context)
+    return render(request, 'datauploader/index.html', context=context)
+
+
+def complete(request):
+    """
+    Receive user from Open Humans. Store data, start upload.
+    """
+    logger.debug("Received user returning from Open Humans.")
+
+    # Exchange code for token.
+    # This creates an OpenHumansMember and associated user account.
+    code = request.GET.get('code', '')
+    oh_member = oh_code_to_member(code=code)
+
+    if oh_member:
+        # Log in the user.
+        user = oh_member.user
+        login(request, user,
+              backend='django.contrib.auth.backends.ModelBackend')
+
+        # Initiate a data transfer task, then render `complete.html`.
+        xfer_to_open_humans.delay(oh_id=oh_member.oh_id)
+        context = {'oh_id': oh_member.oh_id,
+                   'oh_proj_page': settings.OH_ACTIVITY_PAGE}
+        return render(request, 'datauploader/complete.html',
+                      context=context)
+
+    logger.debug('Invalid code exchange. User returned to starting page.')
+    return redirect('/')
