4c621f232500051e2835e24f305f19a3baafc9c7
Stephanie Unna <steph.unna@gmail.com>
2018-02-20 11:32:15 +0000

    Finishing updating views.py adding open humans member

--- datauploader/views.py
+++ datauploader/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.conf import settings
 from .tasks import xfer_to_open_humans
+from .models import OpenHumansMember
 
 # Set up logging.
 logger = logging.getLogger(__name__)
@@ -46,6 +47,57 @@ def complete(request):
 
     logger.debug('Invalid code exchange. User returned to starting page.')
     return redirect('/')
+
+
+def oh_code_to_member(code):
+    """
+    Exchange code for toek, use this to create and return OpenHumansMember.
+    If a matching OpenHumansMember exists, update and return it.
+    """
+    if settings.OH_CLIENT_SECRET and settings.OH_CLIENT_ID and code:
+        data = {
+            'grant_type': 'authorization_code',
+            'redirect_uri': '{}/complete'.format(settings.APP_BASE_URL),
+            'code': code,
+        }
+        req = requests.post(
+            '{}/oauth2/token/'.format(settings.OH_BASE_URL),
+            data=data,
+            auth=requests.auth.HTTPBasicAuth(
+                settings.OH_CLIENT_ID,
+                settings.OH_CLIENT_SECRET
+            )
+        )
+        data = req.json()
+
+        if 'access_token' in data:
+            oh_id = oh_get_member_data(
+                data['access_token'])['project_member_id']
+            try:
+                oh_member = OpenHumansMember.objects.get(oh_id=oh_id)
+                logger.debug('Member {} re-authorized.'.format(oh_id))
+                oh_member.access_token = data['access_token']
+                oh_member.refresh_token = data['refresh_token']
+                oh_member.token_expires = OpenHumansMember.get_expiration(
+                    data['expires_in'])
+            except OpenHumansMember.DoesNotExist:
+                oh_member = OpenHumansMember.create(
+                    oh_id=oh_id,
+                    access_token=data['access_token'],
+                    refresh_token=data['refresh_token'],
+                    expires_in=data['expires_in'])
+                logger.debug('Member {} created.'.format(oh_id))
+            oh_member.save()
+
+            return oh_member
+
+        elif 'error' in req.json():
+            logger.debug('Error in token exchange: {}'.format(req.json()))
+        else:
+            logger.warning('Neither token nor error info in OH response!')
+    else:
+        logger.error('OH_CLIENT_SECRET or code are unavailable')
+    return None
 
 
 def oh_get_member_data(token):
