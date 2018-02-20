ea331d3f578a6ef1bef0dffb9474f730978cd875
Stephanie Unna <steph.unna@gmail.com>
2018-02-20 15:06:57 +0000

    Finishing off OpenHumansMember model and associated methods

--- datauploader/models.py
+++ datauploader/models.py
@@ -1,5 +1,9 @@
+import arrow
+import requests
+from datetime import timedelta
 from django.contrib.auth.models import User
 from django.db import models
+from django.conf import settings
 
 
 class OpenHumansMember(models.Model):
@@ -7,6 +11,61 @@ class OpenHumansMember(models.Model):
     Store OAuth2 data for Open Humans member.
     A user account is created for this Open Humans member.
     """
+    user = models.OneToOneField(User)
+    oh_id = models.CharField(max_length=16, primary_key=True, unique=True)
+    access_token = models.CharField(max_length=256)
+    refresh_token = models.CharField(max_length=256)
+    token_expires = models.DateTimeField()
+
+    @staticmethod
+    def get_expiration(expires_in):
+        return (arrow.now() + timedelta(seconds=expires_in)).format()
+
+    @classmethod
+    def create(cls, oh_id, access_token, refresh_token, expires_in):
+        new_username = make_unique_username(
+            base='{}_openhumans'.format(oh_id))
+        new_user = User(username=new_username)
+        new_user.save()
+        oh_member = cls(
+            user=new_user,
+            oh_id=oh_id,
+            access_token=access_token,
+            refresh_token=refresh_token,
+            token_expires=cls.get_expiration(expires_in))
+        return oh_member
+
+    def __str__(self):
+        return "<OpenHumansMember(oh_id='{}')>".format(
+            self.oh_id)
+
+    def get_access_token(self):
+        """
+        Return access token. Refresh first if necessary.
+        """
+        # Also refresh if nearly expired (less than 60s remaining).
+        delta = timedelta(seconds=60)
+        if arrow.get(self.token_expires) - delta < arrow.now():
+            self._refresh_tokens()
+        return self.access_token
+
+    def _refresh_tokens(self):
+        """
+        Refresh access token.
+        """
+        response = requests.post(
+            'https://www.openhumans.org/oauth2/token/',
+            data={
+                'grant_type': 'refresh_token',
+                'refresh_token': self.refresh_token},
+            auth=requests.auth.HTTPBasicAuth(
+                settings.OH_CLIENT_ID, settings.OH_CLIENT_SECRET))
+        if response.status_code == 200:
+            data = response.json()
+            self.access_token = data['access_token']
+            self.refresh_token = data['refresh_token']
+            self.token_expires = self.get_expiration(data['expires_in'])
+            self.save()
 
 
 def make_unique_username(base):
