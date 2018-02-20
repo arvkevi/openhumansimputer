870c03d5b8c2b241e4f27b22873e3b2528884f61
Stephanie Unna <steph.unna@gmail.com>
2018-02-20 11:55:42 +0000

    Updating models.py starting make unique username

--- datauploader/models.py
+++ datauploader/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+def make_unique_username(base):
+    try:
+        User.objects.get(username=base)
+    except User.DoesNotExist:
+        return base
+    n = 2
+    while True:
+        name = base + str(n)
+        try:
+            User.objects.get(username=name)
+            n += 1
+        except User.DoesNotExist:
+            return name
