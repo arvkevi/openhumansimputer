5abbad9deedeea6dee3ef535053004bd33e2bda3
Stephanie Unna <steph.unna@gmail.com>
2018-02-21 12:08:47 +0000

    Simplifying url paths

--- datauploader/urls.py
+++ datauploader/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('datauploader/index/', views.index, name='index'),
-    path('datauploader/complete/', views.complete, name='complete'),
+    path('', views.index, name='index'),
+    path('complete/', views.complete, name='complete'),
 ]
