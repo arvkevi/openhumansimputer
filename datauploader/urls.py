eb0c62bc24be752cb66989017a7d3e032569a45b
Stephanie Unna <steph.unna@gmail.com>
2018-02-21 12:07:08 +0000

    Removing extra comma in urls.py

--- datauploader/urls.py
+++ datauploader/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('complete/', views.complete, name='complete'),
-],
+    path('datauploader/index/', views.index, name='index'),
+    path('datauploader/complete/', views.complete, name='complete'),
+]
