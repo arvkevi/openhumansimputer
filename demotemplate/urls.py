e776bb6dfd08ef3df955fd8895627d1a56f551f3
Stephanie Unna <steph.unna@gmail.com>
2018-02-23 11:41:52 +0000

    Removing datauploader from path since just one app now.

--- demotemplate/urls.py
+++ demotemplate/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    path('datauploader/', include('datauploader.urls')),
+    path('', include('datauploader.urls')),
     path('admin/', admin.site.urls),
 ]
