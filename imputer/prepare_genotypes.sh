9d3cb97f48fea67107757215372fb607beda5b0d
arvkevi <arvkevi@gmail.com>
2018-10-03 00:13:28 +0000

    replace /home/kevin

--- imputer/prepare_genotypes.sh
+++ imputer/prepare_genotypes.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # drop duplicate records
-uniq "$HOME"/data/"$1"/member."$1".vcf > "$HOME"/data/"$1"/member."$1".uniq.vcf
+uniq "$DATA_DIR"/"$1"/member."$1".vcf > "$DATA_DIR"/"$1"/member."$1".uniq.vcf
 # convert to plink format
-"$HOME"/impbin/plink \
---vcf "$HOME"/data/"$1"/member."$1".uniq.vcf \
+"$IMP_BIN"/plink \
+--vcf "$DATA_DIR"/"$1"/member."$1".uniq.vcf \
 --impute-sex ycount \
 --make-bed \
---out "$HOME"/data/"$1"/member."$1".plink
+--out "$DATA_DIR"/"$1"/member."$1".plink
 # remove missing ids
-"$HOME"/impbin/plink \
---bfile "$HOME"/data/"$1"/member."$1".plink \
+"$IMP_BIN"/plink \
+--bfile "$DATA_DIR"/"$1"/member."$1".plink \
 --maf 0.01 \
 --make-bed \
 --set-missing-var-ids @:\#[b37]\$1,\$2 \
---out "$HOME"/data/"$1"/member."$1".plink.gt
+--out "$DATA_DIR"/"$1"/member."$1".plink.gt
