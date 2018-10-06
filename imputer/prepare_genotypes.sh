98696476dbe5e223da051f7376e96bd9aa3fc7dd
arvkevi <arvkevi@gmail.com>
2018-10-06 12:10:51 +0000

    filter missing genotypes

--- imputer/prepare_genotypes.sh
+++ imputer/prepare_genotypes.sh
@@ -6,12 +6,14 @@ uniq "$DATA_DIR"/"$1"/member."$1".vcf > "$DATA_DIR"/"$1"/member."$1".uniq.vcf
 "$IMP_BIN"/plink \
 --vcf "$DATA_DIR"/"$1"/member."$1".uniq.vcf \
 --impute-sex ycount \
+--geno \
 --make-bed \
 --out "$DATA_DIR"/"$1"/member."$1".plink
 # remove missing ids
 "$IMP_BIN"/plink \
 --bfile "$DATA_DIR"/"$1"/member."$1".plink \
 --maf 0.01 \
+--geno \
 --make-bed \
 --set-missing-var-ids @:\#[b37]\$1,\$2 \
 --out "$DATA_DIR"/"$1"/member."$1".plink.gt
