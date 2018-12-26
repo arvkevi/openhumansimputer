f40b8b3bd4568113b927ba31d2d9a7b44ff7f022
arvkevi <arvkevi@gmail.com>
2018-12-26 05:56:43 -0500

    add fasta to output_vcf.sh

--- imputer/output_vcf.sh
+++ imputer/output_vcf.sh
@@ -10,6 +10,7 @@ sed -i'.bak' '3s/0/-9/g' "$OUT_DIR"/"$1"/chr"$2"/chr"$2"/final_impute2/chr"$2".i
 --sample "$OUT_DIR"/"$1"/chr"$2"/chr"$2"/final_impute2/chr"$2".imputed.sample \
 --hard-call-threshold 0.4 \
 --export vcf \
---ref-from-fa "$REF_FA"/hg19.fasta \
+--ref-from-fa 'force' \
+--fa "$REF_FA"/hg19.fasta \
 --missing-code -9 \
 --out "$OUT_DIR"/"$1"/chr"$2"/chr"$2"/final_impute2/chr"$2".member.imputed
